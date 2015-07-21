1dc3a68865525519880f7b1d9f990ddaf076da5f
Tracy Norris <tknorris@gmail.com>
2015-07-21 02:09:35 -0400

    Add 2.9.6 of SMU to Repo

--- script.module.urlresolver/lib/urlresolver/plugins/openload.py
+++ script.module.urlresolver/lib/urlresolver/plugins/openload.py
@@ -1,6 +1,6 @@
-'''
-vidzi urlresolver plugin
-Copyright (C) 2014 Eldorado
+"""
+openload.io urlresolver plugin
+Copyright (C) 2015 tknorris
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,50 +14,47 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-'''
+"""
 
 from t0mm0.common.net import Net
 from urlresolver.plugnplay.interfaces import UrlResolver
 from urlresolver.plugnplay.interfaces import PluginSettings
 from urlresolver.plugnplay import Plugin
-import re
-import urllib2
 from urlresolver import common
+import re
 
-class VidziResolver(Plugin, UrlResolver, PluginSettings):
+class OpenLoadResolver(Plugin, UrlResolver, PluginSettings):
     implements = [UrlResolver, PluginSettings]
-    name = "vidzi"
-    domains = ["vidzi.tv"]
+    name = "openload"
+    domains = ["openload.io"]
 
     def __init__(self):
         p = self.get_setting('priority') or 100
         self.priority = int(p)
         self.net = Net()
+        self.pattern = '//((?:www.)?openload\.io)/(?:embed|f)/([0-9a-zA-Z-_]+)'
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         html = self.net.http_GET(web_url).content
-        print html
-
-        if '404 Not Found' in html:
-            raise UrlResolver.ResolverError('File Not Found or removed')
-
-        r = re.search('file\s*:\s*"([^"]+)', html)
-        if r:
-            return r.group(1) + '|Referer=http://vidzi.tv/nplayer/jwplayer.flash.swf'
-        else:
-            raise UrlResolver.ResolverError('Unable to locate link')
+        if 'We are sorry!' in html:
+            raise UrlResolver.ResolverError('File Not Found or Removed.')
+        
+        match = re.search('<source\s+type="video/mp4"\s+src="([^"]+)', html)
+        if match:
+            return match.group(1) + '|User-Agent=%s' % (common.IE_USER_AGENT)
+        
+        raise UrlResolver.ResolverError('Unable to resolve openload.io link. Filelink not found.')
 
     def get_url(self, host, media_id):
-        return 'http://%s/embed-%s.html' % (host, media_id)
+            return 'http://openload.io/embed/%s' % (media_id)
 
     def get_host_and_id(self, url):
-        r = re.search('http://(?:www\.|embed-)?(.+?)/(?:embed-)?([0-9a-zA-Z/]+)', url)
+        r = re.search(self.pattern, url)
         if r:
             return r.groups()
         else:
             return False
 
     def valid_url(self, url, host):
-        if self.get_setting('enabled') == 'false': return False
-        return (re.match('http://(www\.|embed-)?vidzi.tv/(?:embed-)?[0-9A-Za-z]+', url) or 'vidzi' in host)
+        return re.search(self.pattern, url) or self.name  in host
