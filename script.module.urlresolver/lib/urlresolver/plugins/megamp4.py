c64b5a4e62286bfaf95c46435a5db5e74bff5181
tknorris <tknorris@tknorris-laptop>
2016-06-27 00:33:47 -0400

    Add 3.0.14 of SMU to Repo

--- script.module.urlresolver/lib/urlresolver/plugins/megamp4.py
+++ script.module.urlresolver/lib/urlresolver/plugins/megamp4.py
@@ -1,6 +1,6 @@
-"""
+'''
     urlresolver XBMC Addon
-    Copyright (C) 2011 t0mm0
+    Copyright (C) 2016 Gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,19 +14,16 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+'''
 
 import re
-from lib import jsunpack
 from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
 
-MAX_TRIES = 3
-
-class VidUpMeResolver(UrlResolver):
-    name = "vidup.me"
-    domains = ["vidup.me", "beta.vidup.me"]
-    pattern = '(?://|\.)(vidup\.me)/(?:embed-)?([0-9a-zA-Z]+)'
+class MegaMP4Resolver(UrlResolver):
+    name = "megamp4.net"
+    domains = ["megamp4.net"]
+    pattern = '(?://|\.)(megamp4\.net)/(?:embed-|emb\.html\?)([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
@@ -34,25 +31,18 @@ class VidUpMeResolver(UrlResolver):
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
         html = self.net.http_GET(web_url).content
-        best_stream_url = ''
-        max_quality = 0
-        for match in re.finditer('(eval\(function.*?)</script>', html, re.DOTALL):
-            js_data = jsunpack.unpack(match.group(1))
-            js_data = js_data.replace("\\'", "'")
-            r = re.findall(r"label\s*:\s*'([^']+)p'\s*,\s*file\s*:\s*'([^']+)", js_data)
-            if r:
-                for quality, stream_url in r:
-                    if int(quality) >= max_quality:
-                        best_stream_url = stream_url
-                        max_quality = int(quality)
 
-            if best_stream_url:
-                return best_stream_url
+        if 'Not Found' in html:
+            raise ResolverError('File Removed')
 
-            raise ResolverError('File Not Found or removed')
+        link = re.search('file:"(.*?)",', html)
+        if link:
+            return link.group(1)
+            
+        raise ResolverError('Unable to find megamp4 video')
 
     def get_url(self, host, media_id):
-        return 'http://beta.vidup.me/embed-%s.html' % media_id
+        return 'http://megamp4.net/embed-%s.html' % (media_id)
 
     def get_host_and_id(self, url):
         r = re.search(self.pattern, url)
