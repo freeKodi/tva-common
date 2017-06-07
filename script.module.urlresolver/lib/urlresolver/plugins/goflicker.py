da44438d284508bd829a1cbc8819ed83b579d2cc
tknorris <tknorris@new-laptop>
2017-06-07 15:59:05 -0400

    Add 3.0.32 of SMU

--- script.module.urlresolver/lib/urlresolver/plugins/goflicker.py
+++ script.module.urlresolver/lib/urlresolver/plugins/goflicker.py
@@ -1,6 +1,6 @@
-"""
-    urlresolver XBMC Addon
-    Copyright (C) 2011 t0mm0
+'''
+    urlresolver Kodi plugin
+    Copyright (C) 2017 Gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -14,40 +14,34 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
+'''
 
-import re
-import urlparse
 from lib import helpers
 from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
+import re
 
-
-class YourUploadResolver(UrlResolver):
-    name = "yourupload.com"
-    domains = ["yourupload.com", "yucache.net"]
-    pattern = '(?://|\.)(yourupload\.com|yucache\.net)/(?:watch|embed)?/?([0-9A-Za-z]+)'
+class GoFlickerResolver(UrlResolver):
+    name = "goflicker.com"
+    domains = ["goflicker.com"]
+    pattern = '(?://|\.)(goflicker\.com)/(?:embed-)?([0-9a-zA-Z]+)'
 
     def __init__(self):
         self.net = common.Net()
 
     def get_media_url(self, host, media_id):
         web_url = self.get_url(host, media_id)
+        headers = {'User-Agent': common.FF_USER_AGENT}
+        response = self.net.http_GET(web_url, headers=headers)
+        html = response.content
+        if 'Not Found' in html:
+            raise ResolverError('File Removed')
 
-        html = self.net.http_GET(web_url).content
-        url = re.findall('file\s*:\s*(?:\'|\")(.+?)(?:\'|\")', html)
+        if 'Video is processing' in html:
+            raise ResolverError('File still being processed')
 
-        if not url: raise ResolverError('No video found')
-
-        headers = {'User-Agent': common.FF_USER_AGENT, 'Referer': web_url}
-
-        url = urlparse.urljoin(web_url, url[0])
-        url = self.net.http_HEAD(url, headers=headers).get_url()
-
-        url = url + helpers.append_headers(headers)
-        return url
-
-        raise ResolverError('No video found')
+        strurl = re.findall('sources.*"(.*?)"',html)[0]
+        return strurl + helpers.append_headers(headers)
 
     def get_url(self, host, media_id):
-        return 'http://www.yourupload.com/embed/%s' % media_id
+        return self._default_get_url(host, media_id)
--- script.module.urlresolver/lib/urlresolver/plugins/vidoza.py
+++ script.module.urlresolver/lib/urlresolver/plugins/vidoza.py
@@ -1,6 +1,6 @@
 """
-    urlresolver XBMC Addon
-    Copyright (C) 2016 lambda
+    Kodi urlresolver plugin
+    Copyright (C) 2016  script.module.urlresolver
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,21 +15,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import re
-from lib import helpers
-from urlresolver import common
-from urlresolver.resolver import UrlResolver, ResolverError
+from __generic_resolver__ import GenericResolver
 
-class UstreamResolver(UrlResolver):
-    name = 'ustream.tv'
-    domains = ['ustream.tv']
-    pattern = '(?://|\.)(ustream\.tv)/embed/([^/?="]+)'
-    
-    def __init__(self):
-        self.net = common.Net()
-        
-    def get_media_url(self, host, media_id):
-        return self.get_url(host, media_id)
-
-    def get_url(self, host, media_id):
-        return 'http://iphone-streaming.ustream.tv/uhls/%s/streams/live/iphone/playlist.m3u8' % media_id
+class VidozaResolver(GenericResolver):
+    name = 'vidoza'
+    domains = ['vidoza.net']
+    pattern = '(?://|\.)(vidoza\.net)/(?:embed-)?([0-9a-zA-Z]+)'
